e91ad683615ced3df4aacfb20da05fbf78bdb6cd
DanielSuescun <suosdacha@gmail.com>
2022-09-24 00:29:18 -0500

    :zap: Se agrega firebase al proyecto, tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
+import { initializeApp } from "firebase/app";
+import PrimeVue from 'primevue/config';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-_Mv6q4nDouEHeFyY_qIwp70nH6rNGoU",
+  authDomain: "dashboard-udi.firebaseapp.com",
+  projectId: "dashboard-udi",
+  storageBucket: "dashboard-udi.appspot.com",
+  messagingSenderId: "849741078840",
+  appId: "1:849741078840:web:a7e39d196c602a1517a1cd",
+  measurementId: "G-E9PR3N0T2W"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
+app.use(PrimeVue);
 
 app.use(createPinia())
 app.use(router)
